db7d1e8774cf0bc9c8ecc8a1885377c70c966e63
mediv <mediv@mail.ru>
2024-05-15 20:33:25 +0300

    users

--- lessons/views.py
+++ lessons/views.py
@@ -1,8 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
 
-from lessons.models import Course, Lesson
-from lessons.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
+from lessons.models import Course, Lesson, Payments
+from lessons.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer, PaymentsSerializer
 
 
 class CourseViewSet(ModelViewSet):
@@ -34,6 +34,31 @@ class LessonRetrieveAPIView(RetrieveAPIView):
     serializer_class = LessonSerializer
 
 
-class LessonDestroyAPIView(DestroyAPIView):
+class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+
+class PaymentsCreateApiView(CreateAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+
+class PaymentsListApiView(ListAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+
+class PaymentsUpdateApiView(UpdateAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+
+class PaymentsRetrieveApiView(RetrieveAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+
+class PaymentsDestroyApiView(DestroyAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
